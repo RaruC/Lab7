85178693c5e1bed6040a18fc0a02d789373cbda7
RaruC <91005152+RaruC@users.noreply.github.com>
2021-11-21 11:27:10 +0200

    Create Cube.java

---
+++ Cube.java
@@ -0,0 +1,18 @@
+package Lab7;
+
+public class Cube implements GeometricBody {
+    private float length;
+
+    public Cube(float l) {
+        length = l;
+    }
+
+    public float getSurface() {
+        return 6 * (length * length);
+    }
+
+    public float getVolume() {
+        return length * length * length;
+    }
+
+}
